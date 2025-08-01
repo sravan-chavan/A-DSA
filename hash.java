4bb969d1011fad879575fe017eeff4bf3b7dec66
sravan-chavan <163759203+sravan-chavan@users.noreply.github.com>
2025-08-01 10:09:33 +0530

    Add files via upload

--- hash.java
+++ hash.java
@@ -1,3 +1,5 @@
+// This code is a hashing of a string and displaying in a hash table
+
 import java.util.*;
 
 public class hash
@@ -5,20 +7,34 @@ public class hash
     public static void main(String [] args)
     {
         Scanner sc = new Scanner(System.in);
-
+        //taking string input from the user
         System.out.print("Enter a string to find its ASCII values :");
-        String name = sc.next();
-        System.out.println("Your give string is : "+name);
-        int asciivalue = hashfunc(name);
-        System.out.println("The ASCII value of the string" +name+ "is = "+asciivalue);
+        String name = sc.nextLine();
+        int sl = name.length();
+        int x=0;
+        for(int i = 0; i<sl; i++)
+        {
+            x=x+ascii(name.charAt(i));
+        }
+        hashfunc(name,x);
     }
-    public static int hashfunc(String name)
+    public static char ascii(char a)
     {
-       int hashvalue=0;
-       for(int i=0 ; i<name.length() ; i++)
+        int ascvalue=a;
+        return a;
+    }
+    public static void hashfunc(String name, int x)
+    {
+       Scanner sq = new Scanner(System.in);
+       System.out.println("Enter the value of hashtable");
+       int n = sq.nextInt();
+
+       int j=x%n;
+       String[] hash = new String[n];
+       hash[j]=name;
+       for(int i = 0; i<n; i++)
        {
-        hashvalue = hashvalue + (int) name.charAt(i);
-       } 
-       return hashvalue;
+        System.out.println(i+"->"+hash[i]);
+       }
     }
 }
